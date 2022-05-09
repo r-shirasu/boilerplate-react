761359c7e37bc7df6d7fc48116cbca5510f282a0
r-shirasu <risako.shirasu@asia-quest.jp>
2022-05-09 23:28:08 +0900

    webpack-devserver 途中

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export const App = () => (
+  <div>
+    <h1>Hello React!</h1>
+  </div>
+);
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 3000,
+  },
+};
