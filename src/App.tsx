ff4e5f4c8a4cc06530d46f2507cb79e27d18bc19
r-shirasu <risako.shirasu@asia-quest.jp>
2022-05-15 22:28:49 +0900

    fix

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export const App = () => (
-  <div>
-    <h1>Hello React!</h1>
-  </div>
-);
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { App } from './App';
+import * as React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const App = () => (
+  <div>
+    <h1>Hello React!</h1>
+  </div>
+);
+
+const rootElement = document.getElementById('app');
+const root = createRoot(rootElement!);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
