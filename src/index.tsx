5a44c2f6e8676a2697e9c3b13c54ed7420e39ecb
r-shirasu <risako.shirasu@asia-quest.jp>
2022-05-15 23:32:29 +0900

    scss導入途中

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import styles from './style.scss';
 
 const App = () => (
   <div>
-    <h1>Hello React!</h1>
+    <h1>
+      Hello <span className={styles.react}>React!</span>
+    </h1>
   </div>
 );
 
-const rootElement = document.getElementById('app');
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
 
 root.render(
